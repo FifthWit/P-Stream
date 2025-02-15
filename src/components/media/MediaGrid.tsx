0494cfb35cce9ceed8a31956dcc815ca7903061b
FifthWit <fifthwitbusiness@gmail.com>
2025-02-15 08:47:55 -0600

    Readded the animation just without the staggering

--- src/components/media/MediaGrid.tsx
+++ src/components/media/MediaGrid.tsx
@@ -1,4 +1,5 @@
-import { forwardRef } from "react";
+import { motion } from "motion/react";
+import { Children, forwardRef, isValidElement } from "react";
 
 interface MediaGridProps {
   children?: React.ReactNode;
@@ -6,13 +7,43 @@ interface MediaGridProps {
 
 export const MediaGrid = forwardRef<HTMLDivElement, MediaGridProps>(
   (props, ref) => {
+    const animatedChildren = Children.map(props.children, (child) => {
+      if (isValidElement(child)) {
+        return (
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 20, scale: 0.7 },
+              show: { opacity: 1, y: 0, scale: 1 },
+            }}
+          >
+            {child}
+          </motion.div>
+        );
+      }
+      return child;
+    });
     return (
-      <div
+      <motion.div
         className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4"
         ref={ref}
+        variants={{
+          hidden: { opacity: 0, y: 20 },
+          show: {
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: "spring",
+              stiffness: 350,
+              damping: 15,
+              mass: 0.4,
+            },
+          },
+        }}
+        initial="hidden"
+        animate="show"
       >
-        {props.children}
-      </div>
+        {animatedChildren}
+      </motion.div>
     );
   },
 );
