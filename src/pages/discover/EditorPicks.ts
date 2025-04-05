f7bb0f2e9c07c6f53941b575aef77cb215de1647
FifthWit <110744759+FifthWit@users.noreply.github.com>
2025-04-04 22:59:16 -0500

    Create EditorPicks.ts

---
+++ src/pages/discover/EditorPicks.ts
@@ -0,0 +1,54 @@
+interface EditorPick {
+  id: number;
+  type: "movie" | "show";
+}
+
+export const EDITOR_PICKS_MOVIES: EditorPick[] = [
+  { id: 9342, type: "movie" }, // The Mask of Zorro
+  { id: 293, type: "movie" }, // A River Runs Through It
+  { id: 370172, type: "movie" }, // No Time To Die
+  { id: 661374, type: "movie" }, // The Glass Onion
+  { id: 207, type: "movie" }, // Dead Poets Society
+  { id: 378785, type: "movie" }, // The Best of the Blues Brothers
+  { id: 335984, type: "movie" }, // Blade Runner 2049
+  { id: 13353, type: "movie" }, // It's the Great Pumpkin, Charlie Brown
+  { id: 27205, type: "movie" }, // Inception
+  { id: 106646, type: "movie" }, // The Wolf of Wall Street
+  { id: 334533, type: "movie" }, // Captain Fantastic
+  { id: 693134, type: "movie" }, // Dune: Part Two
+  { id: 765245, type: "movie" }, // Swan Song
+  { id: 264660, type: "movie" }, // Ex Machina
+  { id: 92591, type: "movie" }, // Bernie
+  { id: 976893, type: "movie" }, // Perfect Days
+  { id: 13187, type: "movie" }, // A Charlie Brown Christmas
+  { id: 11527, type: "movie" }, // Excalibur
+  { id: 120, type: "movie" }, // LOTR: The Fellowship of the Ring
+  { id: 157336, type: "movie" }, // Interstellar
+  { id: 762, type: "movie" }, // Monty Python and the Holy Grail
+  { id: 666243, type: "movie" }, // The Witcher: Nightmare of the Wolf
+  { id: 545611, type: "movie" }, // Everything Everywhere All at Once
+  { id: 329, type: "movie" }, // Jurrassic Park
+  { id: 330459, type: "movie" }, // Rogue One: A Star Wars Story
+  { id: 279, type: "movie" }, // Amadeus
+  { id: 823219, type: "movie" }, // Flow
+];
+
+export const EDITOR_PICKS_TV_SHOWS: EditorPick[] = [
+  { id: 456, type: "show" }, // The Simpsons
+  { id: 73021, type: "show" }, // Disenchantment
+  { id: 1434, type: "show" }, // Family Guy
+  { id: 1695, type: "show" }, // Monk
+  { id: 1408, type: "show" }, // House
+  { id: 93740, type: "show" }, // Foundation
+  { id: 60625, type: "show" }, // Rick and Morty
+  { id: 1396, type: "show" }, // Breaking Bad
+  { id: 44217, type: "show" }, // Vikings
+  { id: 90228, type: "show" }, // Dune Prophecy
+  { id: 13916, type: "show" }, // Death Note
+  { id: 71912, type: "show" }, // The Witcher
+  { id: 61222, type: "show" }, // Bojack Horseman
+  { id: 93405, type: "show" }, // Squid Game
+  { id: 87108, type: "show" }, // Chernobyl
+  { id: 2190, type: "show" }, // South Park
+  { id: 246, type: "show" }, // Avatar: The Last Airbender
+];
