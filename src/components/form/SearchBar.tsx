31a50624eb6b1e5087555e5b71cf661b6dd00c16
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-05-31 09:52:16 -0600

    update blurs

--- src/components/form/SearchBar.tsx
+++ src/components/form/SearchBar.tsx
@@ -40,7 +40,7 @@ export const SearchBarInput = forwardRef<HTMLInputElement, SearchBarProps>(
       <div ref={containerRef}>
         <Flare.Base
           className={c({
-            "hover:flare-enabled group flex flex-col rounded-[28px] transition-colors sm:flex-row sm:items-center relative backdrop-blur-lg":
+            "hover:flare-enabled group flex flex-col rounded-[28px] transition-colors sm:flex-row sm:items-center relative backdrop-blur-sm":
               true,
             "transition-colors duration-300": true,
             "bg-search-background/50": !focused && lightTheme,
