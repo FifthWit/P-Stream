6fa0b08e5ef5e9c6b2e5bd2d361213601c2b7143
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-05-31 18:08:28 -0600

    Update Dropdown.tsx

--- src/components/form/Dropdown.tsx
+++ src/components/form/Dropdown.tsx
@@ -15,12 +15,15 @@ interface DropdownProps {
   setSelectedItem: (value: OptionItem) => void;
   options: Array<OptionItem>;
   direction?: "up" | "down";
+  side?: "left" | "right";
   customButton?: React.ReactNode;
+  customMenu?: React.ReactNode;
   className?: string;
+  preventWrap?: boolean;
 }
 
 export function Dropdown(props: DropdownProps) {
-  const { direction = "down", customButton } = props;
+  const { direction = "down", customButton, customMenu } = props;
 
   return (
     <div className={`relative my-4 w-fit max-w-[25rem] ${props.className}`}>
@@ -48,28 +51,34 @@ export function Dropdown(props: DropdownProps) {
             <Transition
               animation="slide-down"
               show={open}
-              className={`absolute z-[102] min-w-[200px] max-h-60 overflow-auto rounded-lg bg-dropdown-background py-1 text-white shadow-lg ring-1 ring-black ring-opacity-5 scrollbar-thin scrollbar-track-background-secondary scrollbar-thumb-type-secondary focus:outline-none ${
+              className={`absolute z-[102] min-w-[20px] w-fit max-h-60 overflow-auto rounded-lg bg-dropdown-background py-1 text-white shadow-lg ring-1 ring-black ring-opacity-5 scrollbar-thin scrollbar-track-background-secondary scrollbar-thumb-type-secondary focus:outline-none ${
                 direction === "up" ? "bottom-full mb-4" : "top-full mt-1"
-              } w-full`}
+              } ${props.side === "right" ? "right-0" : "left-0"}`}
             >
-              <Listbox.Options static className="py-1">
-                {props.options.map((opt) => (
-                  <Listbox.Option
-                    className={({ active }) =>
-                      `cursor-pointer flex gap-4 items-center relative select-none py-2 px-4 mx-1 rounded-lg ${
-                        active
-                          ? "bg-background-secondaryHover text-type-link"
-                          : "text-type-secondary"
-                      }`
-                    }
-                    key={opt.id}
-                    value={opt}
-                  >
-                    {opt.leftIcon ? opt.leftIcon : null}
-                    {opt.name}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
+              {customMenu ? (
+                <Listbox.Options static as={Fragment}>
+                  {customMenu}
+                </Listbox.Options>
+              ) : (
+                <Listbox.Options static className="py-1">
+                  {props.options.map((opt) => (
+                    <Listbox.Option
+                      className={({ active }) =>
+                        `cursor-pointer flex gap-4 items-center relative select-none py-2 px-4 mx-1 rounded-lg ${
+                          active
+                            ? "bg-background-secondaryHover text-type-link"
+                            : "text-type-secondary"
+                        } ${props.preventWrap ? "whitespace-nowrap" : ""}`
+                      }
+                      key={opt.id}
+                      value={opt}
+                    >
+                      {opt.leftIcon ? opt.leftIcon : null}
+                      {opt.name}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              )}
             </Transition>
           </>
         )}
